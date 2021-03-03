fe1f3cce49bcdfa8dc18f0426de9e2fccea75a02
Alberto <albertogr578@gmail.com>
2021-03-03 14:22:43 +0100

    Añade los objetos creados a un arraylist y los recorre con un foreach

--- src/main/java/tareaclase5f/Prueba.java
+++ src/main/java/tareaclase5f/Prueba.java
@@ -5,33 +5,57 @@
  */
 package tareaclase5f;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author GR6
  */
 public class Prueba {
-    
+
     public static void main(String[] args) {
+
+        // En una clase Prueba, crea 15 objetos CuadradoMagico
+        CuadradoMagico cuadrado1 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado2 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado3 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado4 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado5 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado6 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado7 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado8 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado9 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado10 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado11 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado12 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado13 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado14 = new CuadradoMagico(3);
+        CuadradoMagico cuadrado15 = new CuadradoMagico(3);
+
+        ArrayList<CuadradoMagico> listaCuadrados = new ArrayList<>();
+
+        listaCuadrados.add(cuadrado1);
+        listaCuadrados.add(cuadrado2);
+        listaCuadrados.add(cuadrado3);
+        listaCuadrados.add(cuadrado4);
+        listaCuadrados.add(cuadrado5);
+        listaCuadrados.add(cuadrado6);
+        listaCuadrados.add(cuadrado7);
+        listaCuadrados.add(cuadrado8);
+        listaCuadrados.add(cuadrado9);
+        listaCuadrados.add(cuadrado10);
+        listaCuadrados.add(cuadrado11);
+        listaCuadrados.add(cuadrado12);
+        listaCuadrados.add(cuadrado13);
+        listaCuadrados.add(cuadrado14);
+        listaCuadrados.add(cuadrado15);
+
         
-       // En una clase Prueba, crea 15 objetos CuadradoMagico
-       
-       CuadradoMagico cuadrado1 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado2 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado3 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado4 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado5 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado6 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado7 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado8 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado9 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado10 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado11 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado12 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado13 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado14 = new CuadradoMagico(3);
-       CuadradoMagico cuadrado15 = new CuadradoMagico(3);
-       
-       
+        for (CuadradoMagico listaCuadrado : listaCuadrados) {
+            
+            System.out.println(listaCuadrado.imprimirCuadradoMagico()); 
+            
+        }
     }
-    
+
 }
