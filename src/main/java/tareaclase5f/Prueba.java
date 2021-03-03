c658a67b8ac2425f88b6b10631b42709cbdbd140
Alberto <albertogr578@gmail.com>
2021-03-03 14:10:33 +0100

    Crea la clase Prueba y crea 15 objetos CuadradoMagico

---
+++ src/main/java/tareaclase5f/Prueba.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaclase5f;
+
+/**
+ *
+ * @author GR6
+ */
+public class Prueba {
+    
+    public static void main(String[] args) {
+        
+       // En una clase Prueba, crea 15 objetos CuadradoMagico
+       
+       CuadradoMagico cuadrado1 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado2 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado3 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado4 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado5 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado6 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado7 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado8 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado9 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado10 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado11 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado12 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado13 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado14 = new CuadradoMagico(3);
+       CuadradoMagico cuadrado15 = new CuadradoMagico(3);
+       
+       
+    }
+    
+}
