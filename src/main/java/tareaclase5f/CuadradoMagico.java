ce69d50644fa6160c90503d45d147af8b5297de9
Alberto <albertogr578@gmail.com>
2021-03-03 13:06:31 +0100

    Crea la clase CuadradoMagico

---
+++ src/main/java/tareaclase5f/CuadradoMagico.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tareaclase5f;
+
+/**
+ *
+ * @author GR6
+ */
+public class CuadradoMagico {
+    
+}
